a2bcff69fbb3641de92ed984abad00a3a97a32ba
jin0yoon <wwwz4805@kakao.com>
2020-05-18 15:10:22 +0900

    feat : Update MainActivity
    feat : Update MainActivity
    
    MainActivity contains list fragment if this is first creation or shows the project detail fragment.

--- app/src/main/java/kr/co/jin0yoon/mvvm_practice/view/ui/MainActivity.java
+++ app/src/main/java/kr/co/jin0yoon/mvvm_practice/view/ui/MainActivity.java
@@ -1,16 +1,49 @@
 package kr.co.jin0yoon.mvvm_practice.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-import kr.co.jin0yoon.mvvm_practice.R;
+import javax.inject.Inject;
 
-public class MainActivity extends AppCompatActivity {
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+import kr.co.jin0yoon.mvvm_practice.R;
+import kr.co.jin0yoon.mvvm_practice.service.model.Project;
+
+public class MainActivity extends AppCompatActivity implements HasSupportFragmentInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add project list fragment if this is first creation
+        if (savedInstanceState == null) {
+            ProjectListFragment fragment = new ProjectListFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, fragment, ProjectListFragment.TAG).commit();
+        }
+    }
+
+    /** Shows the project detail fragment */
+    public void show(Project project) {
+        ProjectFragment projectFragment = ProjectFragment.forProject(project.name);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack("project")
+                .replace(R.id.fragment_container,
+                        projectFragment, null).commit();
+    }
+
+    @Override
+    public DispatchingAndroidInjector<Fragment> supportFragmentInjector() {
+        return dispatchingAndroidInjector;
     }
 }
