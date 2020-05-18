9e6c303ea2536313d5ac00a1f933852fae93c735
jin0yoon <wwwz4805@kakao.com>
2020-05-18 09:37:18 +0900

    Create ProjectClickCallback.java

---
+++ app/src/main/java/kr/co/jin0yoon/mvvm_practice/view/callback/ProjectClickCallback.java
@@ -0,0 +1,8 @@
+package kr.co.jin0yoon.mvvm_practice.view.callback;
+
+
+import kr.co.jin0yoon.mvvm_practice.service.model.Project;
+
+public interface ProjectClickCallback {
+    void onClick(Project project);
+}
